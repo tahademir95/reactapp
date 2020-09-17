9e999184bbefb8c963e090bd9c59d666c303453e
tahademir95 <tahadmr.1995@gmail.com>
2020-09-17 17:18:03 +0300

    Yaşam Döngüsü - Update ve Unmounting işlemi

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 
 class Test extends Component {
+    // life cycle da ilk constructor, sonra render, ardından componentDidMount, shouldComponentUpdate ve son olarak componentDidUpdate işlemi sırayla çalışır. Ancak componentDidMount içinde setstate yapılırsa
+    //tekrar render işlemi devreye girer
+    //projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
     constructor(props) {
         super(props);
         this.state = {
@@ -13,6 +16,14 @@ class Test extends Component {
         //Api istekleri burda gerçekleşir
         //state set edilebilir
         console.log("componentDidMount")
+    }
+    componentDidUpdate = (prevProps, prevState) => {
+        //state, props değiştiğinde veya forceUpdate olduğunda çalışır
+        console.log("componentDidUpdate")
+    }
+    componentWillUnmount() {
+        //Component kaldırılmadan hemen önce çalışır.
+        console.log("componentWillUnmount")
     }
 
     render() {
