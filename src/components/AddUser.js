e59bbc92e788a8e81dda9302c66a1a10c39ab6f5
tahademir95 <tahadmr.1995@gmail.com>
2020-09-17 15:39:19 +0300

    Controlled Component - state değerini input değerine girdiğin değer yapmak için

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -17,15 +17,23 @@ const Animation = posed.div({
 });
 class AddUser extends Component {
     state = {
-        isVisible: false
+        isVisible: false,
+        name : "",
+        department : "",
+        salary : ""
     }
 
     changeVisibility = (e) => {
         this.setState({isVisible: !this.state.isVisible})
     }
+    changeInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
 
     render() {
-        const {isVisible} = this.state;
+        const {isVisible, name, department, salary} = this.state;
         return (
             <div className="col-md-8 mb-4">
                 <button className="btn btn-dark btn-block mb-2" onClick={this.changeVisibility}>{isVisible ? "Hide Form" : "Show Form"}</button>
@@ -38,15 +46,15 @@ class AddUser extends Component {
                             <form>
                                 <div className="form-group">
                                     <label htmlFor="name">Name</label>
-                                    <input type="text" name="name" id="id" placeholder="Enter Name" className="form-control"/>
+                                    <input type="text" name="name" id="id" value={name} onChange={this.changeInput} placeholder="Enter Name" className="form-control"/>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="department">Department</label>
-                                    <input type="text" name="department" id="department" placeholder="Enter Department" className="form-control"/>
+                                    <input type="text" name="department" id="department" value={department} onChange={this.changeInput} placeholder="Enter Department" className="form-control"/>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="salary">Salary</label>
-                                    <input type="text" name="salary" id="salary" placeholder="Enter Salary" className="form-control"/>
+                                    <input type="text" name="salary" id="salary" value={salary} onChange={this.changeInput} placeholder="Enter Salary" className="form-control"/>
                                 </div>
                                 <button type="submit" className="btn btn-danger btn-block">Add User</button>
                             </form>
